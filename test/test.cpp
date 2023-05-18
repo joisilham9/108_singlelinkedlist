2d3c32d45032eacd51de879ba71bfd64dcb1542e
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-18 14:56:03 +0700

    menjalankan method addNobe

--- test/test.cpp
+++ test/test.cpp
@@ -4,5 +4,19 @@ using namespace std;
 struct node {
 	int noMhs;
 	string name;
-	node* next
+	node* next;
 };
+
+node* STRAT = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	node* nodeBaru = new node();
+	cout << "masukan NIM";
+	cin >> nim;
+	cout << "masukan NAMA";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+}
