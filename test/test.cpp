b96c87197c6ef3c40fd16cbf0efaa3066804141d
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-05-18 17:09:42 +0700

    perintah default

--- test/test.cpp
+++ test/test.cpp
@@ -176,10 +176,16 @@ void addNode() {
 					break;
 				case 5:
 					break;
-
+				default:
+					cout << "pilihan tidak ada " << endl;
+					break;
 				}
 			}
-		}
+			catch (exception e) 
+			{
+				cout << "terjadi kesalahan" << endl;
+			}
+		} while (pilihan != 5);
 	}
 
 
